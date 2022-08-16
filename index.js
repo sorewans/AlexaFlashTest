641161e654073447c79a3664b7395ea67e6dc15f
Ewan Ross <sorewans@gmail.com>
2022-08-16 14:39:26 +0100

    minor updates

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.get('/', (req, res) => {
     [
       {
           "uid": "c8f4c1ad-7a78-45ad-99f4-299efc53eb56",
-          "updateDate": "2022-16-08T11:28:00.0Z",
+          "updateDate": "2022-16-08T11:28:00Z",
           "titleText": "School Timetable",
           "mainText": "Today is Tuesday so you have English, Maths, Geography, CDT and science",
           "streamUrl": "https://bbc.co.uk",
