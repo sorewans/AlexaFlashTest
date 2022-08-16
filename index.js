31dcd12ae07aa46d7d253d398963c69635e00d17
Ewan Ross <sorewans@gmail.com>
2022-08-16 15:01:46 +0100

    minor updates

--- index.js
+++ index.js
@@ -11,13 +11,19 @@ server.get('/', (req, res) => {
   res.type('application/json').json(
     [
       {
-          "uid": "c8f4c1ad-7a78-45ad-99f4-299efc53eb56",
+          "uid": "c8f4c1ad-7a78-45ad-99f4-299efc53eb58",
           "updateDate": "2022-16-08T11:28:00Z",
           "titleText": "School Timetable",
           "mainText": "Today is Tuesday so you have English, Maths, Geography, CDT and science",
-          "streamUrl": "https://bbc.co.uk",
           "redirectionUrl": "https://bbc.co.uk"
-      }
+      },
+      {
+        "uid": "c8f4c1ad-7a78-45ad-99f4-299efc53eb57",
+        "updateDate": "2022-16-08T11:28:00Z",
+        "titleText": "School Timetable",
+        "mainText": "Tomorrow is Wednesday so you have gym, music, maths, english and history",
+        "redirectionUrl": "https://bbc.co.uk"
+    } 
   ]
   )
 });
